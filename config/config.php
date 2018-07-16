591a3b182080f9fbd5eca4f65552778b92f9e4b5
John Patrick Galacgac <johnpatrick.galacgac@yahoo.com.ph>
2018-07-17 05:03:16 +0800

    Update Config.php and add Fix mysql.inc for mysqli

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 ////For Localhost: Enable This
-$databaseHost = '127.0.0.1';
+$databaseHost = 'localhost';
 $databaseName = 'bsuepayslip';
 $databaseUsername = 'root';
 $databasePassword = '';
@@ -18,7 +18,7 @@ $databasePassword = '';
 		die("Could not connect" . mysql_error());
 	}
 	
- $db = mysql_select_db($databaseName);
+  $db = mysql_select_db($databaseName,$conn);
   if(!$db)
     {
 		die("Could not select database" . mysql_error());
